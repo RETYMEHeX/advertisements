1634f61d30909da4f0bb100fd2f58b3caa3682c5
RETYMEHeX <jikistan123890@mail.ru>
2023-07-30 18:17:16 +0300

    4 lesson

--- mysite/app_advertisements/models.py
+++ mysite/app_advertisements/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
 
 class Advertisement(models.Model):
@@ -8,6 +10,14 @@ class Advertisement(models.Model):
     is_auction = models.BooleanField("Уместен ли торг", help_text="Отметьте если торг по обЪявлению уметсен.")
     updated_at = models.DateTimeField("Дата обновления", auto_now=True)
     created_at = models.DateTimeField("Дата публикации", auto_now_add=True)
+    @admin.display(description="Дата публикации")
+    def created_date(self):
+        from django.utils import timezone
+
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html("<span style='color: green; font-weight: bold;'>Сегодня в {}</span>", created_time)
+        return self.created_at.strftime("%d.%m.%Y - %H:%M")
 
     class Meta:
         db_table = "advertisements"
