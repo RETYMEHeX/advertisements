0b974f45a382b9b9b61b8a26ff34e653a32a2bc3
RETYME <jikistan123890@mail.ru>
2023-08-27 13:14:25 +0300

    Make top label and created registration

--- mysite/myauth/urls.py
+++ mysite/myauth/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import my_login, profile
+from .views import my_login, profile, register
 
 urlpatterns = [
     path("myauth/", my_login, name="login"),
     path("profile/", profile, name='profile'),
+    path('register/', register, name='register')
 ]
--- mysite/myauth/views.py
+++ mysite/myauth/views.py
@@ -1,27 +1,44 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect
 from django.http.response import HttpResponse
 from django.contrib.auth import authenticate, login, logout
-
-# Create your views here.
+from django.urls import reverse
+from .forms import CustomUserCreationForm
 
 def my_login(request):
-    redirect_url = reverse("profile")
-    if request.user.is_authenticated:
-        return redirect(redirect_url)
+    redirect_url = reverse("main")
     if request.method == "GET":
-        return render(request, 'myauth/login.html')
+        if not request.user.is_authenticated:
+            return render(request, 'myauth/login.html')
+        return redirect(redirect_url)
+
     username = request.POST['username']
     password = request.POST['password']
+
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-        return redirect('main')
+        return redirect(redirect_url)
     return render(request, 'myauth/login.html', {"error": "Пользователь не найден"})
 
 def profile(request):
     return render(request, 'myauth/profile.html')
 
 def my_logout(request):
-    redirect_to = reverse("main")
-    logout(request, request.user)
-    return redirect(redirect_to)
+    login_url = reverse("login")
+    logout(request)
+    return redirect(login_url)
+
+def register(request):
+    print(request.POST)
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = CustomUserCreationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'myauth/register.html', context)
